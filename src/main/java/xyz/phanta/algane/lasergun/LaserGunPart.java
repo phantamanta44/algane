e6ab7ea397f683ff12fa6acc8f43f476d20427dd
phantamanta44 <evandalong@gmail.com>
2019-07-29 23:03:18 -0500

    Only allow energy cells that can both receive and provide energy

--- src/main/java/xyz/phanta/algane/lasergun/LaserGunPart.java
+++ src/main/java/xyz/phanta/algane/lasergun/LaserGunPart.java
@@ -2,9 +2,11 @@ package xyz.phanta.algane.lasergun;
 
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.energy.IEnergyStorage;
 import xyz.phanta.algane.init.AlganeCaps;
 
 import javax.annotation.Nullable;
+import java.util.Objects;
 
 public enum LaserGunPart {
 
@@ -20,7 +22,10 @@ public enum LaserGunPart {
             } else if (stack.hasCapability(AlganeCaps.LASER_GUN_MOD, null)) {
                 return MODIFIER;
             } else if (stack.hasCapability(CapabilityEnergy.ENERGY, null) && !stack.hasCapability(AlganeCaps.LASER_GUN, null)) {
-                return ENERGY_CELL;
+                IEnergyStorage energy = Objects.requireNonNull(stack.getCapability(CapabilityEnergy.ENERGY, null));
+                if (energy.canExtract() && energy.canReceive()) {
+                    return ENERGY_CELL;
+                }
             }
         }
         return null;
