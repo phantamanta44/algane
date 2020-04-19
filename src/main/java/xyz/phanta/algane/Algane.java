86a08bfb5a352c07156f45ee96bba85867753694
phantamanta44 <evandalong@gmail.com>
2020-04-19 12:54:17 -0500

    Bump up to 1.0.2, add line intersection check to hitscan (fixes #2)

--- src/main/java/xyz/phanta/algane/Algane.java
+++ src/main/java/xyz/phanta/algane/Algane.java
@@ -14,7 +14,7 @@ import xyz.phanta.algane.item.ItemLaserGun;
 public class Algane extends Virtue {
 
     public static final String MOD_ID = "algane";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     @SuppressWarnings("NotNullFieldNotInitialized")
     @Mod.Instance(MOD_ID)
--- src/main/java/xyz/phanta/algane/util/LasingUtils.java
+++ src/main/java/xyz/phanta/algane/util/LasingUtils.java
@@ -10,7 +10,6 @@ import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
@@ -44,7 +43,7 @@ public class LasingUtils {
 
     public static <E extends Entity> Optional<E> getFirstEntityOnLine(Class<E> entityType, Predicate<Entity> filter,
                                                                       World world, Vec3d from, Vec3d to) {
-        return getEntitiesOnLine(world, from, to).stream()
+        return getEntitiesOnLine(world, from, to)
                 .filter(entityType::isInstance)
                 .map(entityType::cast)
                 .filter(filter)
@@ -53,8 +52,29 @@ public class LasingUtils {
                 .map(IPair::getA);
     }
 
-    public static List<Entity> getEntitiesOnLine(World world, Vec3d from, Vec3d to) {
-        return world.getEntitiesInAABBexcluding(null, new AxisAlignedBB(from.x, from.y, from.z, to.x, to.y, to.z), null);
+    public static Stream<Entity> getEntitiesOnLine(World world, Vec3d from, Vec3d to) {
+        return world.getEntitiesInAABBexcluding(null, new AxisAlignedBB(from.x, from.y, from.z, to.x, to.y, to.z), null)
+                .stream().filter(e -> intersectsLine(e.getEntityBoundingBox().grow(0.5D), from, to));
+    }
+
+    public static boolean intersectsLine(AxisAlignedBB prism, Vec3d lineMin, Vec3d lineMax) {
+        double x1 = lineMin.x, x2 = lineMax.x, dx = x2 - x1;
+        double y1 = lineMin.y, y2 = lineMax.y, dy = y2 - y1;
+        double z1 = lineMin.z, z2 = lineMax.z, dz = z2 - z1;
+        double dydx = dy / dx, dzdx = dz / dx, dzdy = dz / dy;
+        double inter1, inter2;
+        // project to xy plane
+        inter1 = (prism.minX - x1) * dydx + y1;
+        inter2 = (prism.maxX - x1) * dydx + y1;
+        if (Math.min(inter1, inter2) > prism.maxY || Math.max(inter1, inter2) < prism.minY) return false;
+        // project to xz plane
+        inter1 = (prism.minX - x1) * dzdx + z1;
+        inter2 = (prism.maxX - x1) * dzdx + z1;
+        if (Math.min(inter1, inter2) > prism.maxZ || Math.max(inter1, inter2) < prism.minZ) return false;
+        // project to yz plane
+        inter1 = (prism.minY - y1) * dzdy + z1;
+        inter2 = (prism.maxY - y1) * dzdy + z1;
+        return !(Math.min(inter1, inter2) > prism.maxZ || Math.max(inter1, inter2) < prism.minZ);
     }
 
     @Nullable
